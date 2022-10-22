a5a9978023c70664badcd1bd3d54585ea25b0ca1
Alameen8739 <allialameen8739@gmail.com>
2022-10-22 21:24:01 +0100

    update

--- setup/app.js
+++ setup/app.js
@@ -43,3 +43,25 @@ const img = document.getElementById("person-img");
 const author = document.getElementById("author");
 const job = document.getElementById("job");
 const info = document.getElementById("info");
+
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
+const randomBtn = document.querySelector(".random-btn");
+
+// set starting item
+let currentItem = 0;
+
+// load initial item
+window.addEventListener("DOMContentLoaded", function () {
+  showPerson(currentItem);
+});
+
+// show person based on item
+
+function showPerson(person) {
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
